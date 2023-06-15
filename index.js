fd37ad1c1b948082d716f986ccfbb0e0a9110025
IrinaKolganova <irene1805@yandex.ru>
2023-06-16 00:29:11 +0300

    updated index.js file

--- index.js
+++ index.js
@@ -9,17 +9,61 @@ const userChat = document.getElementById('userChat');
 
 function getUserName() {
     const newName = name.value[0].toUpperCase() + name.value.substring(1).toLowerCase();
-    return (userName.textContent = newName);
+    const p = document.createElement('p');
+    p.textContent = newName;
+    userName.appendChild(p);
+    name.value = '';
 }
 
 function checkSpam() {
     const antiSpam = chat.value.replace(/viagra|xxx|ххх/gi, '***');
-    return (userChat.textContent = antiSpam);
+    const antiP = document.createElement('p');
+    antiP.textContent = antiSpam;
+    userName.appendChild(antiP);
+    chat.value = '';
+}
+
+function getPic() {
+    userPhoto.src = photo.value;
+    // const newPic = document.createElement('img');
+    // newPic.src = photo.value;
+    // userPhoto.appendChild(newPic);
+    photo.value = '';
 }
 
 function getComment() {
     getUserName();
+    getPic();
     checkSpam();
-    userPhoto.src = photo.value;
+
 }
 button.addEventListener('click', getComment);
+
+
+// 1я версия кода:
+
+// const name = document.getElementById('name');
+// const photo = document.getElementById('photo');
+// const chat = document.getElementById('chat');
+// const button = document.getElementById('button');
+
+// const userName = document.getElementById('userName');
+// const userPhoto = document.getElementById('userPhoto');
+// const userChat = document.getElementById('userChat');
+
+// function getUserName() {
+//     const newName = name.value[0].toUpperCase() + name.value.substring(1).toLowerCase();
+//     return (userName.textContent = newName);
+// }
+
+// function checkSpam() {
+//     const antiSpam = chat.value.replace(/viagra|xxx|ххх/gi, '***');
+//     return (userChat.textContent = antiSpam);
+// }
+
+// function getComment() {
+//     getUserName();
+//     checkSpam();
+//     userPhoto.src = photo.value;
+// }
+// button.addEventListener('click', getComment);
